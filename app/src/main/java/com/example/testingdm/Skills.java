ed1a6fac910c64e49fadafe9a2af736452c14151
CoreyNearing <55998699+CoreyNearing@users.noreply.github.com>
2019-10-29 12:13:45 -0400

    [Testing DM] - Added skills code - Added constructor - Added proficiency detector

--- app/src/main/java/com/example/testingdm/Skills.java
+++ app/src/main/java/com/example/testingdm/Skills.java
@@ -1,5 +1,28 @@
 package com.example.testingdm;
 
-public class Skills extends ValueCalculation {
+public class Skills extends ValueCalculation
+{
+    int l;
+    String name;
+    boolean p;
+
+    //Constructor Method
+    public Skills (String skillName,int level, boolean proficient)
+    {
+        name = skillName;
+        l = level;
+        p = proficient;
+       l=p();
+    }
+
+    //if proficient it adds 2 to the level
+    private int p()
+    {
+        if(p==true)
+        {
+            return l+2;
+        }
+        return l;
+    }
 
 }
