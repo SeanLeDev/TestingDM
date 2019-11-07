bda535a0172282ebc30d2c932113115b87400c60
Sean <seanle216@gmail.com>
2019-11-07 12:45:05 -0500

    [TestingDM]  - New Activity  - User Input start

--- app/src/main/java/com/example/testingdm/ui/main/characterScreen.java
+++ app/src/main/java/com/example/testingdm/ui/main/characterScreen.java
@@ -1,14 +1,15 @@
-package com.example.testingdm;
+package com.example.testingdm.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.testingdm.ui.main.characterScreen;
+import com.example.testingdm.MainActivity;
+import com.example.testingdm.R;
+import com.example.testingdm.characterEdit;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,31 +17,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.example.testingdm.ui.main.SectionsPagerAdapter;
+import com.example.testingdm.ui.main.ui.main.SectionsPagerAdapter;
 
-
-public class MainActivity extends AppCompatActivity {
+public class characterScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_character_screen);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
         configureFabButton();
-
     }
 
     private void configureFabButton() {
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, characterScreen.class));
+                finish();
             }
         });
 
--- app/src/main/java/com/example/testingdm/ui/main/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/testingdm/ui/main/ui/main/PlaceholderFragment.java
@@ -1,12 +1,15 @@
-package com.example.testingdm.ui.main;
+package com.example.testingdm.ui.main.ui.main;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.testingdm.R;
@@ -43,19 +46,14 @@ public class PlaceholderFragment extends Fragment {
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = null;
-        switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
-            case 1:
-                rootView = inflater.inflate(R.layout.fragment_character, container, false);
-                break;
-            case 2:
-                rootView = inflater.inflate(R.layout.fragment_npcs, container, false);
-                break;
-            case 3:
-                rootView = inflater.inflate(R.layout.fragment_table, container, false);
-                break;
-        }
-
-        return rootView;
+        View root = inflater.inflate(R.layout.fragment_character_screen, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 }
