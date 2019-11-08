273d5b23d87220368fcb6c3ae60f2de7f5108ee4
CoreyNearing <55998699+CoreyNearing@users.noreply.github.com>
2019-11-08 14:36:23 -0500

    [Testing DM] -Starting to work on the sqlite database
    -Made the class and put some code in it

--- app/src/main/java/com/example/testingdm/DatabaseHelper.java
+++ app/src/main/java/com/example/testingdm/DatabaseHelper.java
@@ -1,14 +1,17 @@
 package com.example.testingdm;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper
 {
+    private static final String TAG = "DatabaseHelper";
 
     private static final String TABLE_NAME = "Character_Table";
     private static final String COL1 = "ID";
@@ -22,11 +25,31 @@ public class DatabaseHelper extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        String createTable = "CREATE TABLE" + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " + COL2 +" TEXT)";
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(String.format("DROP IF TABLE EXISTS", TABLE_NAME));
+        onCreate(db);
+    }
 
+    public boolean addData(String item){
+        SQLiteDatabase db =  this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2, item);
+
+        Log.d(TAG, "addData: Adding " + item + " to " + TABLE_NAME);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1)
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
