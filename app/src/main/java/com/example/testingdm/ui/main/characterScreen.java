3677126c354fc1ab3a4a4769bfe8fa63bfac3546
Sean <seanle216@gmail.com>
2019-11-11 12:48:56 -0500

    [TestingDM]  - Fixing the second Activity

--- app/src/main/java/com/example/testingdm/ui/main/characterScreen.java
+++ app/src/main/java/com/example/testingdm/ui/main/characterScreen.java
@@ -36,12 +36,9 @@ public class characterScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_screen);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
-        configureFabButton();
+
     }
 
 
