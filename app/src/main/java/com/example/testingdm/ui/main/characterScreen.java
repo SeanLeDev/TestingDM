f95b4ea370a6c4d41a6a28c474a9b4de00ba7365
Sean <seanle216@gmail.com>
2019-11-14 13:55:47 -0500

    [TestingDM]  - Added Character input screen  - Known bugs :Unable to access second activity

--- app/src/main/java/com/example/testingdm/ui/main/characterScreen.java
+++ app/src/main/java/com/example/testingdm/ui/main/characterScreen.java
@@ -1,24 +1,16 @@
 package com.example.testingdm.ui.main;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 
-import com.example.testingdm.MainActivity;
 import com.example.testingdm.R;
-import com.example.testingdm.characterEdit;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-
-import com.example.testingdm.ui.main.ui.main.SectionsPagerAdapter;
 
 
 public class characterScreen extends AppCompatActivity {
@@ -36,9 +28,11 @@ public class characterScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_screen);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
+        tabs.setupWithViewPager(viewPager);
+        //configureFabButton();
     }
 
 
