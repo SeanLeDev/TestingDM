e7673323bac0a3d987ff4f54fdb1f1bb00ee0652
Sean <seanle216@gmail.com>
2019-11-17 18:31:47 -0500

    [TestingDM]  - Screens have been fixed  - Currently working on bonus calculations

--- app/src/main/java/com/example/testingdm/ui/main/ui/main/characterEdit.java
+++ app/src/main/java/com/example/testingdm/ui/main/ui/main/characterEdit.java
@@ -9,6 +9,8 @@ import androidx.lifecycle.ViewModelProviders;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.testingdm.R;
 import com.example.testingdm.ui.main.PageViewModel;
@@ -18,6 +20,20 @@ public class characterEdit extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
+    public int editStorage;
+    public String conversion;
+    public EditText strInput;
+    public EditText chaInput;
+    public EditText wisInput;
+    public EditText conInput;
+    public EditText dexInput;
+    public EditText intInput;
+    private TextView strBonus;
+    private TextView chaBonus;
+    private TextView wisBonus;
+    private TextView conBonus;
+    private TextView dexBonus;
+    private TextView intBonus;
 
     public static characterEdit newInstance(int index) {
         characterEdit fragment = new characterEdit();
@@ -36,6 +52,7 @@ public class characterEdit extends Fragment {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
+        strInput = (EditText) findViewById(R.id.strInput);
     }
 
     @Override
@@ -47,4 +64,15 @@ public class characterEdit extends Fragment {
         return rootView;
     }
 
+    public void bonusDisplay() {
+        conversion = strInput.getText().toString();
+        try {
+            editStorage = Integer.parseInt(conversion);
+        }
+        //  Set up button
+        catch (NumberFormatException nfe) {
+
+        }
+
+    }
 }
