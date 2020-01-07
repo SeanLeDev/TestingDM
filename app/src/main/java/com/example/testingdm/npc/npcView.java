d7555815fcd24069eb255e7a958122552e211d80
Sean <seanle216@gmail.com>
2020-01-07 12:47:47 -0500

    [TestingDM]  - Added the correct name to NPC window  - More work on making the cardview clickable

--- app/src/main/java/com/example/testingdm/npc/npcView.java
+++ app/src/main/java/com/example/testingdm/npc/npcView.java
@@ -36,8 +36,6 @@ public class npcView extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getIDchips();
 
-        sus.isChecked();
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +45,6 @@ public class npcView extends AppCompatActivity {
             }
         });
     }
-
 
     private void getIDchips() {
         Chip sus = findViewById(R.id.suspicious);
