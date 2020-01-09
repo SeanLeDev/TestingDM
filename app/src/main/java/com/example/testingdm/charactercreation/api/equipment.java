e1eb1397ac7724f24d5502f0447a04052cf38e25
Sean <seanle216@gmail.com>
2020-01-09 17:31:15 -0500

    [TestingDM]  - Working on Equipment functionality  - Needs testing but Skills functionality should work

--- app/src/main/java/com/example/testingdm/charactercreation/api/equipment.java
+++ app/src/main/java/com/example/testingdm/charactercreation/api/equipment.java
@@ -14,10 +14,12 @@ public class equipment {
     private String category_range;
 
     private Map<String, String> damage;
+    private Map<String, String> dmgBonus;
+    private Map<String, Map<String, String>> dmgType;
+
     private Cost cost;
-    private damage_type dmgType;
     private String dmgDice;
-    private int dmgBonus;
+    //private int dmgBonus;
 
     private Weapon_Properties properties;
     private int range;
@@ -67,14 +69,6 @@ public class equipment {
         return dmgDice;
     }
 
-    public int getDmgBonus() {
-        return dmgBonus;
-    }
-
-    public damage_type getDmgType() {
-        return dmgType;
-    }
-
     public int getRange() {
         return range;
     }
@@ -95,8 +89,40 @@ public class equipment {
         String url;
     }
 
-    public class damage_type {
-        String url;
-        String name;
+    public String get_id() {
+        return _id;
+    }
+
+    public String getEquipment_category() {
+        return equipment_category;
+    }
+
+    public String getWeapon_category() {
+        return weapon_category;
+    }
+
+    public String getWeapon_range() {
+        return weapon_range;
+    }
+
+    public String getCategory_range() {
+        return category_range;
+    }
+
+    public Map<String, String> getDmgBonus() {
+        return dmgBonus;
+    }
+
+    public Map<String, Map<String, String>> getDmgType() {
+        return dmgType;
+    }
+
+    public Weapon_Properties getProperties() {
+        return properties;
+    }
+
+    public class damageDiceBonus {
+        String damage_dice;
+        String damage_bonus;
     }
 }
