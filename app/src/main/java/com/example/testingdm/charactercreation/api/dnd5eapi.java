00afdf23e203299b7f16fd8bed239118c91e1b4f
corey <corey.nearing@bell.net>
2020-01-10 16:04:38 -0500

    [Testing DM} - Finished all the @GETs in the dnd5e api

--- app/src/main/java/com/example/testingdm/charactercreation/api/dnd5eapi.java
+++ app/src/main/java/com/example/testingdm/charactercreation/api/dnd5eapi.java
@@ -18,5 +18,23 @@ public interface dnd5eapi {
     @GET("classes/{classname}")
     Call<classes> getclasses(@Path("classname") String classname);
 
+    @GET("choices/{choicename}")
+    Call<choices> getchoices(@Path("choicename") String choicename);
+
+    @GET("levels/{levelname}")
+    Call<levels> getlevels(@Path("levelname") String levelname);
+
+    @GET("proficiencies/{proficiencyname}")
+    Call<proficiencies> getproficiency(@Path("proficiencyname") String proficiencyname);
+
+    @GET("races/{racename}")
+    Call<races> getrace(@Path("racename") String racename);
+
+    @GET("skills/{skillname}")
+    Call<skills> getskill(@Path("skillname") String skillname);
+
+    @GET ("subclasses/{subclassname}")
+    Call<subclasses> getsubclass(@Path("subclassname") String subclassname);
+
     //@GET("spells/{spell}")
 }
